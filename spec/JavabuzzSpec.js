46c47b79b14f1f4e7051c707c34bade9e716479f
danjocutler <danjocutler@gmail.com>
2014-10-14 11:23:08 +0100

    Added file structure, and passed first two tests - (not) divisible by three

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,17 @@
+describe('Javabuzz', function() {
+	var javabuzz;
+	describe('knows when a number is', function() {
+		it('divisible by 3', function() {
+			javabuzz = new Javabuzz();
+			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
+		});
+
+	});
+
+	describe('knows when a number is NOT', function() {
+		it('divisible by 3', function() {
+			javabuzz = new Javabuzz();
+			expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+		});
+	});
+});
