3da28fe95c053b77198b50ff5426f9a8029eb3bd
danjocutler <danjocutler@gmail.com>
2014-10-14 11:58:53 +0100

    Added 'Java' 'Buzz' and 'Javbuzz' functions

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -27,4 +27,16 @@ describe('Javabuzz', function() {
 			expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
 		});
 	});
+
+	describe('when playing, says', function() {
+		it('"Java" when a number is divisible by 3', function() {
+			expect(javabuzz.says(3)).toEqual("Java");
+		});
+		it('"Buzz" when a number is divisible by 5', function() {
+			expect(javabuzz.says(5)).toEqual("Buzz");
+		});
+		it('"Java" when a number is divisible by 15', function() {
+			expect(javabuzz.says(15)).toEqual("Javabuzz");
+		});
+	});
 });
